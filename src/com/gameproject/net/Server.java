ab728fe1f69b696066903b38d4bbd60aaacbd4d3
LucasRogeland <lucas@crossfade.se>
2016-01-12 14:22:09 +0100

    Update Server.java

--- src/com/gameproject/net/Server.java
+++ src/com/gameproject/net/Server.java
@@ -41,7 +41,7 @@ public class Server{
 	//Initializes the server and asks for the port
 	private static void init() throws IOException{
 		closeRequested = false;
-		db = new DatabaseConnection("jdbc:mysql://localhost:3306/game", "root", "F2g37c8dp1kk");
+		db = new DatabaseConnection("jdbc:mysql://localhost:3306/game", "root", "password");
 		int port = 0;
 		try{
 			port = Integer.parseInt(JOptionPane.showInputDialog("Server Port"));
